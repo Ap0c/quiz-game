f6f08e60f27ab82a48203db631a64b4a8c06ba86
Ap0c <Ap0c@users.noreply.github.com>
2016-05-18 23:03:42 +0100

    Set up basic socket.io connection.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let express = require('express');
 let http = require('http');
-let io = require('socket.io');
+let socketIO = require('socket.io');
 
 
 // ----- Setup ---- //
@@ -11,7 +11,7 @@ const PORT = 3000;
 
 let app = express();
 let server = http.Server(app);
-let socket = io(server);
+let io = socketIO(server);
 
 
 // ----- Middleware ----- //
@@ -23,6 +23,19 @@ app.use(express.static('static'));
 
 app.get('/', (req, res) => {
 	res.sendFile('index.html', { root: '.' });
+});
+
+
+// ----- Socket Events ----- //
+
+io.on('connection', (socket) => {
+
+	console.log('A player connected.');
+
+	socket.on('disconnect', () => {
+		console.log('A player disconnected.');
+	});
+
 });
 
 
