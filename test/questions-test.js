e028a28932b0e2943f991874925c87d4c731a456
Ap0c <Ap0c@users.noreply.github.com>
2016-05-25 21:41:12 +0100

    Added further tests for questions module, and modified questions tests to work directly with questions json test file.

--- test/questions-test.js
+++ test/questions-test.js
@@ -2,8 +2,9 @@
 
 // ----- Requires ----- //
 
-let expect = require('chai').expect;
-let Questions = require('../lib/questions');
+const expect = require('chai').expect;
+const fs = require('fs');
+const Questions = require('../lib/questions');
 
 
 // ----- Setup ----- //
@@ -11,11 +12,24 @@ let Questions = require('../lib/questions');
 const QUESTIONS_FILE = './test/questions.json';
 
 
+// ----- Functions ----- //
+
+// Loads the questions from file and parses as JSON.
+function loadQuestions (file) {
+
+	let questions = fs.readFileSync(file);
+
+	return JSON.parse(questions);
+
+}
+
+
 // ----- Tests ----- //
 
 describe('Questions', function () {
 
 	let questions = {};
+	let questionList = loadQuestions(QUESTIONS_FILE)
 
 	beforeEach(function () {
 		questions = Questions(QUESTIONS_FILE);
@@ -28,7 +42,7 @@ describe('Questions', function () {
 	it('retrieves a question from a category', function () {
 
 		let question = questions.get('catOne');
-		expect(question).to.eql({q: 'Question One', a: ['Answer One']});
+		expect(question).to.eql(questionList.catOne[0]);
 
 	});
 
@@ -36,6 +50,27 @@ describe('Questions', function () {
 
 		let question = questions.get('invalid');
 		expect(question).to.be.null;
+
+	});
+
+	it('retrieves a question from the used list', function () {
+
+		let freshQuestion = questions.get('catOne');
+		let usedQuestion = questions.get('catOne');
+		
+		expect(usedQuestion).to.eql(questionList.catOne[0]);
+
+	});
+
+	it('retrieves multiple questions', function () {
+
+		let firstQuestion = questions.get('catTwo');
+		let secondQuestion = questions.get('catTwo');
+		let usedQuestion = questions.get('catTwo');
+
+		expect(questionList.catTwo).to.include(firstQuestion);
+		expect(questionList.catTwo).to.include(secondQuestion);
+		expect(questionList.catTwo).to.include(usedQuestion);
 
 	});
 
