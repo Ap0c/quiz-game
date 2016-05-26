3df0cd07e043df3561f510f7343d6c9c18f5ec33
Ap0c <Ap0c@users.noreply.github.com>
2016-05-26 22:09:43 +0100

    Added additional test suite for users module.

---
+++ test/users-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// ----- Requires ----- //
+
+let expect = require('chai').expect;
+let Users = require('../lib/users');
+
+
+// ----- Mockups ----- //
+
+function MockSocket (id) {
+
+	return {
+		id: id,
+		emit: function () {}
+	};
+
+}
+
+
+// ----- Tests ----- //
+
+describe('Users', function () {
+
+	let users = null;
+
+	beforeEach(function () {
+		users = Users();
+	});
+
+	it('fails to update scores on invalid user', function () {
+
+		users.add(MockSocket('an id'), 'Player 1');
+
+		let result = users.updateScore('invalid', 2);
+		expect(result).to.be.null;
+
+	});
+
+});
