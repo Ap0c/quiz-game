db10226b4423451015839c6939e663482934c429
Ap0c <Ap0c@users.noreply.github.com>
2016-05-27 23:49:08 +0100

    Changed first screen to render with mithril.

--- static/app.js
+++ static/app.js
@@ -1,3 +1,22 @@
-// ----- Run ----- //
+// ----- Dependencies ----- //
 
 var socket = io();
+
+
+// ----- User Choice Component ----- //
+
+var chooseUser = {};
+
+chooseUser.controller = function () {};
+
+chooseUser.view = function (ctrl) {
+
+	return [
+		m('button.choose-player', 'Player'),
+		m('button.choose-host', 'Host'),
+		m('button.choose-screen', 'Screen')
+	];
+
+};
+
+m.module(document.getElementsByTagName('main')[0], chooseUser);
