5d646ed8c32f45ca541b20751e7abf2cb223d844
Ap0c <Ap0c@users.noreply.github.com>
2016-05-28 00:47:27 +0100

    Added model to handle users client-side.

--- static/app.js
+++ static/app.js
@@ -3,20 +3,52 @@
 var socket = io();
 
 
+// ----- Models ----- //
+
+var User = function User (name) {
+
+	socket.emit('add-user', name);
+
+	return {
+		name: name
+	};
+
+};
+
+var user = null;
+
+
 // ----- User Choice Component ----- //
 
 var chooseUser = {};
 
-chooseUser.controller = function () {};
+chooseUser.controller = function () {
+
+	// Creates a new user with a given name.
+	function createUser (name) {
+		user = User(name);
+	}
+
+	return {
+		user: createUser
+	};
+
+};
 
 chooseUser.view = function (ctrl) {
 
 	return [
-		m('button.choose-player', 'Player'),
-		m('button.choose-host', 'Host'),
-		m('button.choose-screen', 'Screen')
+		m('button.choose-player', {
+			onclick: function () { ctrl.user('player'); }
+		}, 'Player'),
+		m('button.choose-host', {
+			onclick: function () { ctrl.user('host'); }
+		}, 'Host'),
+		m('button.choose-screen', {
+			onclick: function () { ctrl.user('screen'); }
+		}, 'Screen')
 	];
 
 };
 
-m.module(document.getElementsByTagName('main')[0], chooseUser);
+m.mount(document.getElementsByTagName('main')[0], chooseUser);
